b18a5392c7ebbbff47e32e0ad1820ac3ae692700
Ganeshmehto <ganeshmehto2004@gmail.com>
2025-03-30 11:28:27 +0530

    new add ons

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,18 +1,24 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import asyncHandler from "../middlewares/asyncHandler.js";
+import generateTokenToken from '../utils/createToken.js'
+
+
 const createUser=asyncHandler(async (req,res)=>{
     const {userName,email,password}=req.body;
-    if (!userName || !email || !password) {
+    if (!userName || !email || !password) 
         console.log("enter all reqiued all inputs")
-    }
+    
     const userExists=await User.findOne({email })
     if (userExists) res.status(400).send('user already exists')
-        const salt =await bcrypt.genSalt(10)
+
+    const salt =await bcrypt.genSalt(10)
     const hashedPassword=await bcrypt.hash(password,salt)
+    
     const newUser = new User({userName,email,password:hashedPassword})
     try {
         await newUser.save()
+        generateTokenToken(res , newUser._id )
         res.status(201).json({
             _id:newUser._id,
             userName:newUser.username,
