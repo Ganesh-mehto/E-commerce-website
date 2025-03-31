8d44d8ab25c7c81adc716b474031f89de40662d4
Ganeshmehto <ganeshmehto2004@gmail.com>
2025-03-31 11:37:28 +0530

    login and logout controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,33 +1,74 @@
 import User from "../models/userModel.js";
-import bcrypt from "bcryptjs";
 import asyncHandler from "../middlewares/asyncHandler.js";
-import generateTokenToken from '../utils/createToken.js'
+import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
-
-const createUser=asyncHandler(async (req,res)=>{
-    const {userName,email,password}=req.body;
-    if (!userName || !email || !password) 
-        console.log("enter all reqiued all inputs")
-    
-    const userExists=await User.findOne({email })
-    if (userExists) res.status(400).send('user already exists')
-
-    const salt =await bcrypt.genSalt(10)
-    const hashedPassword=await bcrypt.hash(password,salt)
-    
-    const newUser = new User({userName,email,password:hashedPassword})
-    try {
-        await newUser.save()
-        generateTokenToken(res , newUser._id )
-        res.status(201).json({
-            _id:newUser._id,
-            userName:newUser.username,
-            email:newUser.email,
-            isAdmin:newUser.isAdmin
-        })
-    }catch(error){
-        res.status(400);
-        throw new Error("invalid user data")
+const createUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+  
+    if (!username || !email || !password) {
+      throw new Error("Please fill all the inputs.");
     }
-})
-export {createUser}
+  
+    const userExists = await User.findOne({ email });
+    if (userExists) res.status(400).send("User already exists");
+  
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({ username, email, password: hashedPassword });
+  
+    try {
+      await newUser.save();
+      createToken(res, newUser._id);
+  
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+      });
+    } catch (error) {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  });
+  
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+  
+  
+    const existingUser = await User.findOne({ email });
+  
+    if (existingUser) {
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+  
+      if (isPasswordValid) {
+        createToken(res, existingUser._id);
+  
+        res.status(201).json({
+          _id: existingUser._id,
+          username: existingUser.username,
+          email: existingUser.email,
+          isAdmin: existingUser.isAdmin,
+        });
+        return;
+      }
+    }
+  });
+
+  const logoutCurrentUser=asyncHandler(async(res,req)=>{
+    res.cokkie('jwt','',{
+        httyOnly:true,
+        expires:new Date(0),
+    })
+    req.status(200).json({message:'logout successfully'})
+  })
+
+export {createUser,loginUser,logoutCurrentUser}  
+
+
+
+
