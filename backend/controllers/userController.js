689c0028be67f181bff7c2c1ad3d83e12229f8b9
Ganeshmehto <ganeshmehto2004@gmail.com>
2025-04-02 22:00:43 +0530

    user routes complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,9 +36,6 @@ const createUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email);
-  console.log(password);
-
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
@@ -97,12 +94,14 @@ const updateCurrentUserProfile = asyncHandler(async (req, res) => {
     user.email = req.user.email || user.email
 
     if (req.body.password) {
-      user.password = req.body.password
+      const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword
     }
     const updatedUser = await user.save()
     res.json({
       _id:updatedUser._id,
-      username:updatedUser.usernmae,
+      username:updatedUser.username,
       emial:updatedUser.email,
       isAdmin:updatedUser.isAdmin,
     })
@@ -112,5 +111,55 @@ const updateCurrentUserProfile = asyncHandler(async (req, res) => {
     throw new Error("user not found")
   }
 })
+const deleteUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
 
-export { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile }
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error("Cannot delete admin user");
+    }
+
+    await User.deleteOne({ _id: user._id });
+    res.json({ message: "User removed" });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const updateUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile,deleteUserById,getUserById,updateUserById}
