f0d7b396fc1e0c5150edb0e47338f77b2adb85f1
Ganeshmehto <ganeshmehto2004@gmail.com>
2025-04-15 10:12:05 +0530

    category backend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js"
 import userRoutes from "./routes/userRouter.js"
+import categoryRoutes from './routes/categoryRoutes.js'
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectDB();
@@ -13,5 +15,6 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use("/api/users",userRoutes)
+app.use('/api/category',categoryRoutes)
 
 app.listen(port,()=>console.log(`server running on port :${port}`));
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import flowbitePlugin from 'flowbite/plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),flowbitePlugin],
   server: {
     proxy: {
       "/api/": "http://localhost:5000",
